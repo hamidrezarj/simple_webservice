29c9273951c17be3702c80d8f736378849931c7b
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2021-01-02 10:13:48 +0330

    Update http method implemented

--- handler/handleReq.go
+++ handler/handleReq.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/hamidrezaRanjbarpour/simple_webservice/model"
@@ -28,13 +29,62 @@ type createResponse struct {
 	Message      string `json:"msg"`
 }
 
+type errorResponse struct {
+	Message string `json:"msg"`
+}
+
 func Hello(c echo.Context) error {
 	var jsonStr = "{\"name\" : \"Hamid\"}"
 
 	return c.String(http.StatusOK, jsonStr)
+
 }
 
 var cnt uint64 = 0
+var customers []interface{}
+
+// returns customer's index in slice if found. else -1
+func findCustomer(Name string, Tel uint64, Address string) (int, model.Customer) {
+
+	for i := 0; i < len(customers); i++ {
+		if customers[i] == nil {
+			continue
+		}
+
+		c, ok := customers[i].(model.Customer)
+
+		if ok {
+
+			if c.Name == Name && c.Tel == Tel && c.Address == Address {
+				return i, c
+			}
+
+		}
+	}
+	return -1, model.Customer{}
+
+}
+
+func findByID(ID uint64) (int, model.Customer) {
+
+	for i := 0; i < len(customers); i++ {
+		if customers[i] == nil {
+			continue
+		}
+
+		c, ok := customers[i].(model.Customer)
+
+		if ok {
+
+			if c.ID == ID {
+				return i, c
+			}
+
+		}
+	}
+	return -1, model.Customer{}
+
+}
 
 func (customer Customer) Create(c echo.Context) error {
 	var req request
@@ -45,26 +95,94 @@ func (customer Customer) Create(c echo.Context) error {
 
 	regDate := time.Now().Format("2006-1-2")
 
-	cnt++
+	if index, _ := findCustomer(req.Name, req.Tel, req.Address); index == -1 {
+		cnt++
 
-	m := model.Customer{
-		Name:         req.Name,
-		Tel:          req.Tel,
-		Address:      req.Address,
-		ID:           cnt,
-		RegisterDate: regDate,
+		m := model.Customer{
+			Name:         req.Name,
+			Tel:          req.Tel,
+			Address:      req.Address,
+			ID:           cnt,
+			RegisterDate: regDate,
+		}
+
+		res := createResponse{
+			Name:         req.Name,
+			Tel:          req.Tel,
+			Address:      req.Address,
+			ID:           cnt,
+			RegisterDate: regDate,
+			Message:      "success",
+		}
+
+		if cap(customers) == 0 {
+			customers = make([]interface{}, 10)
+		}
+
+		if int(cnt-1) == len(customers) {
+			customers = append(customers, m)
+		} else {
+			customers[cnt-1] = m
+		}
+
+		fmt.Println(m)
+		fmt.Println(customers, cap(customers), len(customers))
+		return c.JSON(http.StatusCreated, res)
 	}
 
-	res := createResponse{
-		Name:         req.Name,
-		Tel:          req.Tel,
-		Address:      req.Address,
-		ID:           cnt,
-		RegisterDate: regDate,
-		Message:      "success",
+	res := errorResponse{
+		Message: "Customer already exists.",
 	}
 
-	fmt.Println(m)
+	return echo.NewHTTPError(http.StatusBadRequest, res)
 
-	return c.JSON(http.StatusCreated, res)
+}
+
+func (customer Customer) Update(c echo.Context) error {
+	var req request
+
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	fmt.Println(c.ParamNames(), c.ParamValues())
+
+	id := c.Param("id")
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, errorResponse{
+			Message: "Please enter valid url.",
+		})
+	}
+
+	if index, cus := findByID(uint64(intID)); index != -1 {
+		//do some shit
+
+		cus.Name = req.Name
+		cus.Tel = req.Tel
+		cus.Address = req.Address
+
+		res := createResponse{
+			Name:         req.Name,
+			Tel:          req.Tel,
+			Address:      req.Address,
+			ID:           cus.ID,
+			RegisterDate: cus.RegisterDate,
+			Message:      "success",
+		}
+
+		customers[index] = cus
+		fmt.Println(customers)
+
+		return c.JSON(http.StatusOK, res)
+	}
+
+	res := errorResponse{
+		// Name:    req.Name,
+		// Tel:     req.Tel,
+		// Address: req.Address,
+		Message: "cID is not available. (Customer with this information doesn't exist)",
+	}
+
+	return c.JSON(http.StatusBadRequest, res)
 }
--- webservice.go
+++ webservice.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	// "gorm.io/driver/sqlite"
+	// "gorm.io/gorm"
 
 	"github.com/hamidrezaRanjbarpour/simple_webservice/handler"
 	"github.com/labstack/echo/v4"
@@ -11,13 +13,16 @@ import (
 
 func main() {
 
-	// fmt.Println(time.Now().Year + "-" + time.Now().Month + "-" + time.Now().Day)
-	fmt.Println(time.Now().Format("2006-1-2"))
+	// db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	// if err != nil {
+	// 	panic("failed to connect to database")
+	// }
 
 	e := echo.New()
 
 	e.GET("/hello", handler.Hello)
 	e.POST("/customers", handler.Customer{}.Create)
+	e.PUT("/customers/:id", handler.Customer{}.Update)
 
 	if err := e.Start("0.0.0.0:8080"); err != nil {
 		fmt.Println(err)
